ef61ae125fdd8a427d813fc62bd61f7729e6cdd8
Inayá Ferreira <inayavitoria10@gmail.com>
2021-02-15 23:52:14 -0300

    :package:menusidebar

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburger").addEventListener("click", () => /*Quando houver o movimento click, ele vai executar a função*/
+    document.querySelector(".container").classList.toggle("show-menu")
+);
